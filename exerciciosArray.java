6d8ba63614d54aa39a9d498172c79b45d9b1340c
Eduardo Dias Bianchini <edudiasbianchini@gmail.com>
2024-04-19 20:57:41 -0300

    Update exerciciosArray.java

--- exerciciosArray.java
+++ exerciciosArray.java
@@ -1,17 +1,28 @@
 import javax.swing.JOptionPane;
+import java.util.Random;
 
-public class exerciciosArray {
+public class ArrayInicio {
     
     public static void main(String[] args) {
-      
-        exercicio4();
-      
+
+        //exercicio1();
+        //exercicio2();
+        //exercicio3();
+        //exercicio4();
+        //exercicio5();
+        //exercicio6();
+        //exercicio7();
+        //exercicio8();
+        //exercicio9();
+        //exercicio10();
+        exercicio11();
+
     }
 
     private static void exercicio1(){
 
-        // Exercicio 1: Receba três números digitados pelo usuário (com input), salvando em um array(vetor), 
-        // obtenha a soma dessas variáveis e a média delas, apresentando esses resultados na tela (com optionpane).
+        // Exercicio 1:
+
 
         String var;
         double numeros[] = new double[3];
@@ -77,4 +88,157 @@ public class exerciciosArray {
         JOptionPane.showMessageDialog(null, "Antecessores: " +antecessor1 +" e " +antecessor2);
         JOptionPane.showMessageDialog(null, "Antecessores: " +sucessor1 +" e " +sucessor2);
     }
-}
+
+    private static void exercicio4(){
+
+        // Recebe dois números digitados pelo usuário (com imput), salvando em um array(vetor), 
+        // em seguida apresente na tela (com optionpane) os se os números são positivo, negativo ou zero.
+
+        String var;
+        int numero[] = new int[2];
+
+        var = JOptionPane.showInputDialog("Digite o primeiro número.");
+        numero[0] = Integer.parseInt(var);
+
+        var = JOptionPane.showInputDialog("Digite o segundo número.");
+        numero[1] = Integer.parseInt(var);
+
+        if (numero[0] < 0){
+            JOptionPane.showMessageDialog(null, "O número " +numero[0] +" é negativo.");
+        }
+        else if (numero[0] == 0){
+            JOptionPane.showMessageDialog(null, "O número " +numero[0] +" é 0.");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "O número " +numero[0] +" é positivo.");
+        }
+
+        if (numero[1] < 0){
+            JOptionPane.showMessageDialog(null, "O número " +numero[1] +" é negativo.");
+        }
+        else if (numero[1] == 0){
+            JOptionPane.showMessageDialog(null, "O número " +numero[1] +" é 0.");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "O número " +numero[1] +" é positivo.");
+        }
+    }
+
+    private static void exercicio5(){
+
+        // Exercicio 5: Receba um numero digitado pelo usuário (com imput), 
+        // apos calcular o fatorial do número e apresentar na tela (com optionpane).
+
+        String var;
+        int numero[] = new int[1];
+
+        var = JOptionPane.showInputDialog("Digite um número, iremos calcular seu fatorial.");
+        numero[0] = Integer.parseInt(var);
+
+        int fat = 1;
+
+        for (int i = 1; i <= numero[0]; i++) {
+            fat *= i;
+        }
+
+        JOptionPane.showMessageDialog(null, "O fatorial de " +numero[0] +" é: " +fat);
+    }
+
+    private static void exercicio6(){
+
+        // Exercício 6: Receba uma letra digitado pelo usuário (com imput), verificar 
+        // se um caractere é uma vogal ou uma consoante, em seguida apresente na tela (com optionpane) o resultado.
+
+        String letra;
+
+        letra = JOptionPane.showInputDialog("Digite uma letra."); 
+
+        if (letra.equals("a") || letra.equals("e") || letra.equals("i") ||
+        letra.equals("o") || letra.equals("u") || letra.equals("A") || 
+        letra.equals("E") || letra.equals("I") || letra.equals("O") ||
+        letra.equals("U")){
+            JOptionPane.showMessageDialog(null, "É vogal.");
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "É consoante.");
+        }
+    }
+
+    private static void exercicio7(){
+
+        // Exercicio 7: Receba um ano digitado pelo usuário (com input), verificar se um ano é um ano de copa do mundo 
+        // (considerando a periodicidade de 4 anos, e a primeira copa ocorreu em 1930), em seguida apresente na tela
+        // (com optionpane) o resultado.
+
+        String var;
+        int ano[] = new int[1];
+
+        var = JOptionPane.showInputDialog("Digite algum ano");
+        ano[0] = Integer.parseInt(var);
+
+        if (ano[0] >= 1930 && (ano[0] - 1930) % 4 == 0){
+            JOptionPane.showMessageDialog(null, "É ano de copa");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Não é ano de copa");
+        }
+    }
+
+    private static void exercicio8(){
+
+        // Exercicio 8: Receber altura e peso digitados pelo usuário (com imput), calcular o IMC
+        // em seguida apresente na tela (com optionpane) o resultado.
+
+        String var;
+        double peso, altura;
+
+        var = JOptionPane.showInputDialog("Digite sua altura");
+        altura = Double.parseDouble(var);
+
+        var = JOptionPane.showInputDialog("Digite seu peso");
+        peso =Double.parseDouble(var);
+
+        double imc = peso / (altura * altura);
+
+        JOptionPane.showMessageDialog(null, "O seu IMC é: " +imc);
+    }
+
+    private static void exercicio9(){
+
+        // Exercicio 9: Simular uma calculadora básica com as operações de adição, subtração, multiplicação e divisão.
+
+        String var;
+        double nm[] = new double[2];
+        int conversao;
+
+        var = JOptionPane.showInputDialog("Digite o primeiro número do cálculo");
+        nm[0] = Double.parseDouble(var);
+
+        var = JOptionPane.showInputDialog("Digite o segundo número do cálculo");
+        nm[1] = Double.parseDouble(var);
+
+        var = JOptionPane.showInputDialog("Agora, escolha o cálculo que deseja realizar \n "
+            +"1 - Adição \n"
+            +"2 - Subtração \n"
+            +"3 - Multiplicação \n"
+            +"4 - Divisão");
+        conversao = Integer.parseInt(var);
+
+        switch (conversao) {
+            case 1:
+                JOptionPane.showMessageDialog(null, "Resultado da adição: " + (nm[0] + nm[1]));
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Resultado da subtração: " + (nm[0] - nm[1]));
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Resultado da multiplicação: " + (nm[0] * nm[1]));
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "Resultado da divisão: " + (nm[0] / nm[1]));
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inválida.");
+                break;
+        }
+    }
